077f278860d66a068dba013790e3539c3dcf85bf
unknown <kcroshan682@gmail.com>
2022-09-07 23:00:46 +0545

    many many field solve

--- company/serializers.py
+++ company/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Company, Document, Shareholders
-
-
+# from rest_framework.response import Response
+# from rest_framework import status
 
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,63 +14,36 @@ class ShareholdersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shareholders
         fields = "__all__"
-        read_only_fields = ['id']
+        # read_only_fields = ['id']
 
 
 
 class CompanySerializer(serializers.ModelSerializer):
-    shareholders = ShareholdersSerializer()
+    shareholders = ShareholdersSerializer(many=True)
     # document =  serializers.ListField(child = DocumentSerializer())
     class Meta:
         model = Company
         fields = '__all__'
-        read_only_fields = ['id']
+        # read_only_fields = ['id']
 
-    
-    def create(self, validated_data):
-        shareholders = validated_data.pop('shareholders',[])
+    def validate_shareholders(self,shareholders):
+        if len(shareholders) <= 0:
+            raise serializers.ValidationError("shareholders null data not accepted")
 
-        id= []
-        print("data")
-        
-        # for shareholder in shareholders:
-        #     print(shareholder)
-        #    data =  Shareholders.objects.create(**shareholder)
-        #    print(data.id)
-        #    id.append(data.id)
 
-        # c1:Company = Company.objects.create(**validated_data)
-
-        # # for i in id:
-        #     # print("============")
-        #     # print(i)
-        #     # print("================")
-        # c1.shareholders_id = id
-        # c1.save()
-        # c1.shareholders.set(id)
-            
-
-        # return super().create(validated_data)
-    
-
-      
-   
     # def create(self, validated_data):
+    # '''
+        # yesma reponse ma problem i don't know why
+    # '''
     #     print(validated_data)
-    #     # shareholders = validated_data.pop('shareholders')
-    #     # print("======================")
-    #     # print(shareholders)
-    #     # print("======================")
-    #     # print("======================")
-       
-    #     # documents = validated_data.pop('document')
-        
-
-    #     # #saving shareholder
-    #     # for shareholder in shareholders
-    #     #    data =  Shareholders.objects.create(**shareholder)
-    #     #    print('data',data)
-            
-    #     # Company.objects.create(**validated_data)
-    #     return validated_data
-
+    #     shareholders = validated_data.pop('shareholders',[])
+    #     id= []
+    #     for shareholder in shareholders:
+    #         data =  Shareholders.objects.create(**shareholder)
+    #         id.append(data.id)
+    #     validated_data["shareholders"] = id
+    #     company = Company()
+    #     company.__dict__.update(validated_data)
+    #     company.save()
+    #     company.shareholders.set(id)
+    #     return Response(company, status=status.HTTP_201_CREATED)
